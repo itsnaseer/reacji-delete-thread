64c8a032ba94dc80c47cb37c864b93abad11cc14
Naseer <naseern.rashid@gmail.com>
2024-06-10 13:52:05 -0400

    Update slack_event_handler.py
    Fix syntax error

--- slack_event_handler.py
+++ slack_event_handler.py
@@ -211,4 +211,4 @@ def slack_events():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 3000))
-    app.run(debug=True, host='0.0.0.0
+    app.run(debug=True, host='0.0.0.0', port=port)
