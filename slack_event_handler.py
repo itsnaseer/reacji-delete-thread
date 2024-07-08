4a3a6032a40f7a00797eb27db4d762df48a8bf37
Naseer <naseern.rashid@gmail.com>
2024-07-08 14:40:30 -0400

    Update slack_event_handler.py
    updated scopes for install flow

--- slack_event_handler.py
+++ slack_event_handler.py
@@ -58,7 +58,7 @@ def verify_slack_request(request):
 def install():
     state = str(uuid.uuid4())
     store[state] = time.time()  # store the state with a timestamp
-    scopes = "channels:history,channels:read,chat:write,reactions:read,chat:write.public,emoji:read,users:read,chat:write.customize,im:history,mpim:history,groups:history,im:read,mpim:read,groups:read,users:read.email"
+    scopes = "channels:history,channels:read,chat:write,reactions:read,chat:write.public,emoji:read,users:read,chat:write.customize,im:history,mpim:history,groups:history,im:read,mpim:read,groups:read,users:read.email,chat:delete"
     user_scopes = "users:read,users:read.email"
     oauth_url = f"https://slack.com/oauth/v2/authorize?client_id={os.getenv('SLACK_CLIENT_ID')}&scope={scopes}&user_scope={user_scopes}&state={state}&redirect_uri={os.getenv('REDIRECT_URI')}"
     return redirect(oauth_url)
