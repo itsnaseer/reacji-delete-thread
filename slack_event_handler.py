f853673ec4d5b444d911f0e75ebc9868266fa5f0
Naseer <naseern.rashid@gmail.com>
2024-07-08 15:23:04 -0400

    Update slack_event_handler.py
    more user scopes

--- slack_event_handler.py
+++ slack_event_handler.py
@@ -60,7 +60,7 @@ def install():
     state = str(uuid.uuid4())
     store[state] = time.time()  # store the state with a timestamp
     scopes = "channels:history,channels:read,chat:write,reactions:read,chat:write.public,emoji:read,users:read,chat:write.customize,im:history,mpim:history,groups:history,im:read,mpim:read,groups:read,users:read.email"
-    user_scopes = "users:read,users:read.email,chat:write"
+    user_scopes = "channels:history,channels:read,reactions:read,users:read,users:read.email,chat:write"
     oauth_url = f"https://slack.com/oauth/v2/authorize?client_id={os.getenv('SLACK_CLIENT_ID')}&scope={scopes}&user_scope={user_scopes}&state={state}&redirect_uri={os.getenv('REDIRECT_URI')}"
     return redirect(oauth_url)
 
