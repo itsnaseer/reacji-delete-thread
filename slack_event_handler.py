b01b84ceff50edc8c3e8dfdb7a3b9453d5cbd91f
Naseer <naseern.rashid@gmail.com>
2024-07-11 17:53:53 -0400

    Update slack_event_handler.py
    closed the view

--- slack_event_handler.py
+++ slack_event_handler.py
@@ -93,7 +93,7 @@ def update_home_tab(client, event, logger):
                     ]
                 }
             ]
-            }
+            })
 
     except Exception as e:
         logger.error(f"Error publishing home tab: {e}")
