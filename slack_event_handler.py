673a75660fe5fb615a079738d893b58f92c3f183
Naseer <naseern.rashid@gmail.com>
2024-07-15 16:14:13 -0400

    Update slack_event_handler.py
    fixed app initiation. closed the parenths

--- slack_event_handler.py
+++ slack_event_handler.py
@@ -320,4 +320,4 @@ def slack_events():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 3000))
-    flask_app.run(debug=True, host='0.0.0.0',
+    flask_app.run(debug=True, host='0.0.0.0', port=port)
