595a7c32fc9c2026b8ea088f31e3ba3718b7a449
Naseer <naseern.rashid@gmail.com>
2024-07-29 16:14:10 -0400

    Update custom_installation_store.py
    address the issues related to the select statements in custom_installation_store.py

--- custom_installation_store.py
+++ custom_installation_store.py
@@ -36,13 +36,15 @@ class CustomInstallationStore(InstallationStore):
 
     def find_installation(self, *, enterprise_id=None, team_id=None, is_enterprise_install=None):
         with self.engine.connect() as connection:
-            stmt = select([
-                self.installations.c.enterprise_id,
-                self.installations.c.team_id,
-                self.installations.c.user_id,
-                self.installations.c.access_token,
-                self.installations.c.bot_token
-            ]).where(
+            stmt = select(
+                [
+                    self.installations.c.enterprise_id,
+                    self.installations.c.team_id,
+                    self.installations.c.user_id,
+                    self.installations.c.access_token,
+                    self.installations.c.bot_token
+                ]
+            ).where(
                 self.installations.c.enterprise_id == enterprise_id,
                 self.installations.c.team_id == team_id
             )
@@ -59,11 +61,13 @@ class CustomInstallationStore(InstallationStore):
 
     def find_bot(self, *, enterprise_id=None, team_id=None):
         with self.engine.connect() as connection:
-            stmt = select([
-                self.installations.c.enterprise_id,
-                self.installations.c.team_id,
-                self.installations.c.bot_token
-            ]).where(
+            stmt = select(
+                [
+                    self.installations.c.enterprise_id,
+                    self.installations.c.team_id,
+                    self.installations.c.bot_token
+                ]
+            ).where(
                 self.installations.c.enterprise_id == enterprise_id,
                 self.installations.c.team_id == team_id
             )
